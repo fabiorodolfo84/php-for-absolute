0d2fc69086c6f15fe579e5c24119c3e5fae2d1bc
fabiorodolfo84 <fabiorodolfo84>
2023-09-27 17:25:52 -0300

    update de exercicios

---
+++ instrucoes-echo.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "<h2>Hello world!</h2>";
+echo "<h2>I'm about to learn php!</h2>";
+echo "<h2>This ", "string ", "was ", "made ", "with multiple parameters.</h2>";
+
+
